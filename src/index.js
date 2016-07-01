7330dcffd09b709daec4903447f5e00b0a6a818f
freder <freder.code@gmail.com>
2016-07-01 15:34:33 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Visualization from './Visualization.js';
 import attacktreeVis from './visualizations/attacktree.js';
 import testVis from './visualizations/test.js';
 
-// TODO: don't mix require and es6 modules
+
 module.exports = {
 	Visualization,
 
