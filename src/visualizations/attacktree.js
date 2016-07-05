da2737ac0acaf5988865bbb68bc1cb13c7c879ee
freder <freder.code@gmail.com>
2016-07-05 18:07:49 +0200

    first steps towards collapse / expand

--- src/visualizations/attacktree.js
+++ src/visualizations/attacktree.js
@@ -14,8 +14,18 @@ import theme from '../theme.js';
 
 function styleNode(node, theme) {
 	return node
-		.attr('fill', theme.node.fill)
-		.attr('r', theme.node.radius);
+		.attr('r', theme.node.radius)
+		.style('fill', (d) => {
+			return d.children
+				? theme.node.fill
+				: 'white';
+		})
+		.style('stroke', (d) => {
+			return d.children
+				? 'none'
+				: theme.node.fill;
+		})
+		.style('stroke-width', 3);
 }
 
 
@@ -53,6 +63,79 @@ function edgePath(d) {
 }
 
 
+function draw(rootGroup, h, $rootSelection) {
+	// prepare data
+	const tree = d3Tree()
+		.size([
+			$rootSelection.width() - (2 * theme.padding),
+			$rootSelection.height() - (2 * theme.padding),
+		]);
+	tree(h);
+	const descendants = h.descendants();
+
+	// edges
+	const link = rootGroup.selectAll('.link')
+		.data(R.tail(descendants));
+
+	link
+		.enter()
+			.append('path')
+				.attr('class', 'link')
+				.attr('d', edgePath)
+			.call(styleEdge, theme);
+
+	// link: exit
+	link.exit()
+		.transition(1000)
+			.style('opacity', 0)
+			.remove();
+
+	// nodes
+	const node = rootGroup.selectAll('.node')
+		.data(descendants);
+
+	// node: update
+	node.selectAll('circle')
+		.call(styleNode, theme);
+	node.selectAll('text')
+		.call(styleLabel, theme);
+
+	// node: exit
+	node.exit()
+		.transition(1000)
+			.style('opacity', 0)
+			.remove();
+
+	// node: enter
+	const nodeEnter = node.enter()
+		.append('g')
+			.attr('class', 'node')
+			.attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+
+	// toggle children on click
+	function onClick(d) {
+		// console.log(d);
+		/* eslint no-param-reassign: 0 */
+		if (d.children) {
+			d._children = d.children;
+			d.children = null;
+		} else {
+			d.children = d._children;
+			d._children = null;
+		}
+		draw(rootGroup, h, $rootSelection);
+	}
+	nodeEnter
+		.append('circle')
+			.call(styleNode, theme)
+			.on('click', onClick);
+
+	nodeEnter
+		.append('text')
+			.call(styleLabel, theme);
+}
+
+
 export default {
 	init(rootElem) {
 		const rootSelection = select(rootElem);
@@ -66,16 +149,7 @@ export default {
 	update(rootSelection, attacktree) {
 		const rootGroup = rootSelection.select('.rootGroup');
 		const $rootSelection = $(rootSelection.node());
-
-		// prepare data
 		const h = d3Hierarchy(attacktree.node, (d) => d.node);
-		const descendants = h.descendants();
-		const tree = d3Tree()
-			.size([
-				$rootSelection.width() - (2 * theme.padding),
-				$rootSelection.height() - (2 * theme.padding),
-			]);
-		tree(h);
 
 		// zoom behavior
 		const zoomThreshold = 0.6;
@@ -100,33 +174,11 @@ export default {
 				].join(' ');
 				rootGroup.attr('transform', transformation);
 			});
-		rootSelection.call(zoom);
+		rootSelection
+			.call(zoom)
+			// prevent double-click zoom
+			.on('dblclick.zoom', null);
 
-		// edges
-		const link = rootGroup.selectAll('.link')
-			.data(R.tail(descendants))
-			.enter()
-				.append('path')
-					.attr('class', 'link')
-					.attr('d', edgePath)
-				.call(styleEdge, theme);
-
-		// nodes
-		const node = rootGroup.selectAll('.node')
-			.data(descendants)
-				.enter()
-					.append('g')
-						.attr('class', (d) => {
-							return 'node';
-						})
-						.attr('transform', (d) => {
-							return `translate(${d.x}, ${d.y})`;
-						});
-
-		node.append('circle')
-			.call(styleNode, theme);
-
-		node.append('text')
-			.call(styleLabel, theme);
+		draw(rootGroup, h, $rootSelection);
 	}
 };
