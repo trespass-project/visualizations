e21714ecc60a0d2352a8a9a30dc5c4f339ab2706
freder <freder.code@gmail.com>
2016-07-06 10:46:39 +0200

    refactored

--- src/visualizations/attacktree.js
+++ src/visualizations/attacktree.js
@@ -1,6 +1,6 @@
 import {
 	event as d3Event,
-	select
+	select as d3Select
 } from 'd3-selection';
 import { zoom as d3Zoom } from 'd3-zoom';
 import { transition as d3Transition } from 'd3-transition';
@@ -18,6 +18,134 @@ import theme from '../theme.js';
 const transition = d3Transition()
 	.duration(1000)
 	.ease(easeSinInOut);
+
+
+const visualization = {};
+export default visualization;
+
+
+visualization.init = function(rootElem) {
+	const rootSelection = d3Select(rootElem);
+	const $rootSelection = $(rootSelection.node());
+
+	const rootGroup = rootSelection
+		.append('g')
+			.attr('class', 'rootGroup')
+			.attr('transform', `translate(${theme.padding}, ${theme.padding})`);
+
+	// zoom behavior
+	const zoomThreshold = 0.6;
+	const zoom = d3Zoom()
+		.scaleExtent([0.1, 10])
+		.on('zoom', () => {
+			// https://github.com/d3/d3-zoom#zoomTransform
+			const scaleFactor = d3Event.transform.k;
+			// console.log(scaleFactor);
+
+			if (scaleFactor >= zoomThreshold) {
+				$rootSelection.find('.node text')
+					.css('visibility', 'visible');
+			} else {
+				$rootSelection.find('.node text')
+					.css('visibility', 'hidden');
+			}
+
+			const transformation = [
+				`translate(${d3Event.transform.x}, ${d3Event.transform.y})`,
+				`scale(${d3Event.transform.k})`,
+			].join(' ');
+			rootGroup.attr('transform', transformation);
+		});
+	rootSelection
+		.call(zoom)
+		// prevent double-click zoom
+		.on('dblclick.zoom', null);
+
+	return rootSelection;
+};
+
+
+visualization.update = function(rootSelection, data) {
+	const $rootSelection = $(rootSelection.node());
+	const rootGroup = rootSelection.select('.rootGroup');
+
+	// prepare data
+	// create hierarchy
+	const h = (!data.data)
+		// make hierarchy from original attacktree
+		? d3Hierarchy(data, (d) => d.node)
+		: data;
+	const tree = d3Tree()
+		.size([
+			$rootSelection.width() - (2 * theme.padding),
+			$rootSelection.height() - (2 * theme.padding),
+		]);
+	tree(h);
+	const descendants = h.descendants();
+
+
+	// edges
+	const link = rootGroup.selectAll('.link')
+		.data(R.tail(descendants));
+
+	link
+		.enter()
+			.append('path')
+				.attr('class', 'link')
+				.attr('d', edgePath)
+			.call(styleEdge, theme);
+
+	// link: exit
+	link.exit()
+		.transition(transition)
+			.style('opacity', 0)
+			.remove();
+
+	// nodes
+	const node = rootGroup.selectAll('.node')
+		.data(descendants);
+
+	// node: update
+	node.selectAll('circle')
+		.call(styleNode, theme);
+	node.selectAll('text')
+		.call(styleLabel, theme);
+
+	// node: exit
+	node.exit()
+		.transition(transition)
+			.style('opacity', 0)
+			.remove();
+
+	// node: enter
+	const nodeEnter = node.enter()
+		.append('g')
+			.attr('class', 'node')
+			.attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+
+	// toggle children on click
+	const onClick = (d) => {
+		// console.log(d);
+		/* eslint no-param-reassign: 0 */
+		if (d.children) {
+			d._children = d.children;
+			d.children = null;
+		} else {
+			d.children = d._children;
+			d._children = null;
+		}
+		this.update(rootSelection, h);
+	};
+	nodeEnter
+		.append('circle')
+			.call(styleNode, theme)
+			.on('click', onClick);
+
+	nodeEnter
+		.append('text')
+			.call(styleLabel, theme);
+};
+
 
 function styleNode(node, theme) {
 	return node
@@ -68,124 +196,3 @@ function edgePath(d) {
 		`${d.parent.x}, ${d.parent.y}`,
 	].join(' ');
 }
-
-
-function draw(rootGroup, h, $rootSelection) {
-	// prepare data
-	const tree = d3Tree()
-		.size([
-			$rootSelection.width() - (2 * theme.padding),
-			$rootSelection.height() - (2 * theme.padding),
-		]);
-	tree(h);
-	const descendants = h.descendants();
-
-	// edges
-	const link = rootGroup.selectAll('.link')
-		.data(R.tail(descendants));
-
-	link
-		.enter()
-			.append('path')
-				.attr('class', 'link')
-				.attr('d', edgePath)
-			.call(styleEdge, theme);
-
-	// link: exit
-	link.exit()
-		.transition(transition)
-			.style('opacity', 0)
-			.remove();
-
-	// nodes
-	const node = rootGroup.selectAll('.node')
-		.data(descendants);
-
-	// node: update
-	node.selectAll('circle')
-		.call(styleNode, theme);
-	node.selectAll('text')
-		.call(styleLabel, theme);
-
-	// node: exit
-	node.exit()
-		.transition(transition)
-			.style('opacity', 0)
-			.remove();
-
-	// node: enter
-	const nodeEnter = node.enter()
-		.append('g')
-			.attr('class', 'node')
-			.attr('transform', (d) => `translate(${d.x}, ${d.y})`);
-
-	// toggle children on click
-	function onClick(d) {
-		// console.log(d);
-		/* eslint no-param-reassign: 0 */
-		if (d.children) {
-			d._children = d.children;
-			d.children = null;
-		} else {
-			d.children = d._children;
-			d._children = null;
-		}
-		draw(rootGroup, h, $rootSelection);
-	}
-	nodeEnter
-		.append('circle')
-			.call(styleNode, theme)
-			.on('click', onClick);
-
-	nodeEnter
-		.append('text')
-			.call(styleLabel, theme);
-}
-
-
-export default {
-	init(rootElem) {
-		const rootSelection = select(rootElem);
-		rootSelection
-			.append('g')
-				.attr('class', 'rootGroup')
-				.attr('transform', `translate(${theme.padding}, ${theme.padding})`);
-		return rootSelection;
-	},
-
-	update(rootSelection, attacktree) {
-		const rootGroup = rootSelection.select('.rootGroup');
-		const $rootSelection = $(rootSelection.node());
-		const h = d3Hierarchy(attacktree.node, (d) => d.node);
-
-		// zoom behavior
-		const zoomThreshold = 0.6;
-		const zoom = d3Zoom()
-			.scaleExtent([0.1, 10])
-			.on('zoom', () => {
-				// https://github.com/d3/d3-zoom#zoomTransform
-				const scaleFactor = d3Event.transform.k;
-				// console.log(scaleFactor);
-
-				if (scaleFactor >= zoomThreshold) {
-					$rootSelection.find('.node text')
-						.css('visibility', 'visible');
-				} else {
-					$rootSelection.find('.node text')
-						.css('visibility', 'hidden');
-				}
-
-				const transformation = [
-					`translate(${d3Event.transform.x}, ${d3Event.transform.y})`,
-					`scale(${d3Event.transform.k})`,
-				].join(' ');
-				rootGroup.attr('transform', transformation);
-			});
-		rootSelection
-			.call(zoom)
-			// prevent double-click zoom
-			.on('dblclick.zoom', null);
-
-		draw(rootGroup, h, $rootSelection);
-	}
-};
