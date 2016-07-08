78d1a951004168c9e615c8891bdc0a9d60928a51
freder <freder.code@gmail.com>
2016-07-08 14:44:38 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-
-// import Visualization from './Visualization.js';
 import AttacktreeVisualization from './AttacktreeVisualization.js';
 
 
 module.exports = {
-	// Visualization,
 	AttacktreeVisualization,
 };
