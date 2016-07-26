38d8a7d31df342c513cfa919941d9af36550bb97
freder <freder.code@gmail.com>
2016-07-26 09:33:48 +0200

    component now only takes a scale function as prop

--- src/ColorScale.js
+++ src/ColorScale.js
@@ -1,57 +1,57 @@
 import React, { Component } from 'react';
-const R = require('ramda');
+import autobind from 'class-autobind';
 const _ = require('lodash');
-const { createColorScale } = require('./colors.js');
-import { interpolateLab } from 'd3-interpolate';
 
 
 class ColorScale extends Component {
+	constructor(props) {
+		super(props);
+		autobind(this);
+	}
+
 	render() {
 		const props = this.props;
 
-		const domain = _.range(0, props.values.length);
-		const max = R.last(domain);
 		const numSteps = Math.trunc(props.numSteps);
-		const step = max / (numSteps - 1);
+		const domain = [0, (numSteps - 1)];
+		const scale = props.scale(domain);
 
-		const scale = createColorScale(
-			props.values,
-			props.ts,
-			domain,
-			props.method
-		);
+		function renderStep(i, index) {
+			return <div
+				key={index}
+				style={{
+					display: 'inline-block',
+					background: scale(i),
+					height: props.height,
+					flex: 1,
+				}}
+			/>;
+		}
 
-		return (
-			<div>
-				{_.range(0, numSteps)
-					.map((index) => index * step)
-					.map((t, index) => {
-						return <div
-							key={index}
-							style={{
-								display: 'inline-block',
-								background: scale(t),
-								width: 600 / numSteps,
-								height: 50
-							}}
-						/>;
-					})
-				}
-			</div>
-		);
+		return <div
+			style={{
+				width: props.width,
+				height: props.height,
+				display: 'flex',
+				flexDirection: 'row',
+			}}
+		>
+			{_.range(0, numSteps).map(renderStep)}
+		</div>;
 	}
 }
 
 ColorScale.propTypes = {
-	method: React.PropTypes.func, // d3 interpolation function
-	values: React.PropTypes.array.isRequired, // list of d3-color instances
-	ts: React.PropTypes.array,
+	scale: React.PropTypes.func.isRequired,
 	numSteps: React.PropTypes.number,
+	width: React.PropTypes.number,
+	height: React.PropTypes.number,
 };
 
 ColorScale.defaultProps = {
 	numSteps: 5,
-	method: interpolateLab, // d3 interpolation function
+	width: 600,
+	height: 50,
 };
 
 module.exports = ColorScale;
