3e4c5d9f5c00ac20dd991e07790381005f4c3ae8
freder <freder.code@gmail.com>
2016-10-05 14:46:53 +0200

    clean up

--- src/AttacktreeVisualization.js
+++ src/AttacktreeVisualization.js
@@ -32,16 +32,26 @@ const xSize = 75;
 const ySize = 100;
 
 
+const rad2DegFactor = (180 / Math.PI);
 function rad2Deg(a) {
-	return a * (180 / Math.PI);
+	return a * rad2DegFactor;
 }
 
 
+const deg2RadFactor = (2 * Math.PI) / 360;
 function deg2Rad(a) {
-	return (a / 360) * (2 * Math.PI);
+	return a * deg2RadFactor;
 }
 
 
+function getVectorLength(x, y) {
+	return Math.sqrt(
+		Math.pow(x, 2) + Math.pow(y, 2)
+	);
+}
+
+
+// TODO: `d3-path` already has this functionality
 function line(p1, p2) {
 	return [
 		'M', `${p1.x}, ${p1.y}`,
@@ -50,6 +60,7 @@ function line(p1, p2) {
 }
 
 
+// TODO: `d3-path` already has this functionality
 function pathifyBezier(p1, c1, c2, p2) {
 	return [
 		'M', `${p1.x}, ${p1.y}`,
@@ -63,13 +74,15 @@ function pathifyBezier(p1, c1, c2, p2) {
 function diagonalBezier(p1, p2, dir) {
 	const distX = (p2.x - p1.x);
 	const distY = (p2.y - p1.y);
+
 	if (dir === 'vertical'
 		|| (Math.abs(distX) <= Math.abs(distY))) {
 		const m = p1.y + (distY / 2);
 		const c1 = { x: p1.x, y: m };
 		const c2 = { x: p2.x, y: m };
 		return { p1, c1, c2, p2 };
-	} else if (dir === 'horizontal'
+	}
+	else if (dir === 'horizontal'
 		|| (Math.abs(distX) >= Math.abs(distY))) {
 		const m = p1.x + (distX / 2);
 		const c1 = { x: m, y: p1.y };
@@ -105,12 +118,6 @@ const layouts = {
 				y: x,
 			};
 		},
-		// edgePath: (x1, y1, x2, y2) => {
-		// 	return line(
-		// 		{ x: x1, y: y1 },
-		// 		{ x: x2, y: y2 },
-		// 	);
-		// },
 		edgePath: (x1, y1, x2, y2) => {
 			const { p1, c1, c2, p2 } = diagonalBezier(
 				{ x: x1, y: y1 },
@@ -143,11 +150,6 @@ const layouts = {
 		},
 	},
 };
-
-
-function getVectorLength(x, y) {
-	return Math.sqrt((x * x) + (y * y));
-}
 
 
 function makeZoomBehavior(rootGroup, rootSelection) {
